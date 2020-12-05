86e6e895c6b4793bfdf7cd370c5af89eb2e439fc
MrT3313 <turgeon.dev@gmail.com>
2020-12-05 15:35:48 -0500

    Part 1 completed

--- 2020/Day5/Day5_1.py
+++ 2020/Day5/Day5_1.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def readInputData():
     # Get Input Data
@@ -9,60 +10,60 @@ def readInputData():
 
     return dataArray
 
+def getData(data, fullString):
+
+    def recurse(modifiedData, str):
+        # Base Case
+        if len(modifiedData) == 1:
+            return modifiedData[0]
+
+        if str[0] == "F" or str[0] == "L":
+            midIdx = len(modifiedData) // 2
+            half = modifiedData[:midIdx]
+        if str[0] == "B" or str[0] == "R":
+            midIdx = len(modifiedData) // 2
+            half = modifiedData[midIdx:]
+            
+        return recurse(half, str[1:])
+    
+    result = recurse(data, fullString)
+    
+    return result
 
 def binaryBoarding():
-
+    # Get Data
     data = readInputData()
     dataArray = data.split("\n")
-    num_of_rows = 127
+    num_of_rows = 128
+    num_of_cols = 8
+    result = -math.inf
 
+    # Modify Data
     modifiedArray = [[
         string,
         string[:7], # rowData
         string[7:], # colData
     ] for string in dataArray]
 
+    # Loop through boarding passes
     for boardingPass in modifiedArray:
         # [0] = full string
         # [1] = row data
         # [2] = col data
-        
-        def getRow(data, fullString):
-            print(fullString)
 
-            def recurse(modifiedData, str):
-                # Base Case
-                if len(modifiedData) == 1:
-                    print('GET number')
-                    return modifiedData[0]
+        # Get row and column data
+        row = getData(list(range(0, num_of_rows)),  boardingPass[1])
+        col = getData(list(range(0, num_of_cols)),    boardingPass[2])
 
-                print(modifiedData)
-                print(str)
-                if str[0] == "F":
-                    midIdx = len(modifiedData) // 2
-                    print('MID IDX ', midIdx)
-                    half = modifiedData[:midIdx]
-                    print('HALF -- F', half)
-                    # pass
-                if str[0] == "B":
-                    midIdx = len(modifiedData) // 2
-                    print('MID IDX ', midIdx)
-                    half = modifiedData[midIdx:]
-                    print('HALF -- B', half)
-                    
-                    # exit()
-                return recurse(half, str[1:])
-            
-            result = recurse(data, fullString)
-            return result
+        # Calculate seatID
+        seatID = row * 8 + col
 
-        # row = getRow(list(range(0, 128)), boardingPass[1])
-        row = getRow(list(range(0, 128)), 'FBFBBFF')
-        print(row)
-        exit()
+        # Update max seatID
+        if seatID > result:
+            result = seatID
 
-    return 'BLARG'
+    return result
 
 # TEST 
-result = binaryBoarding()
-print(result)
+# result = binaryBoarding()
+# print(result)
