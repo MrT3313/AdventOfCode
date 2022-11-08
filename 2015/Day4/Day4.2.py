156af0c6e53650c092f5249ae1f2ce214098cc2e
Reed Turgeon <turgeon.dev@gmail.com>
2022-11-07 23:26:39 -0700

    2015 day 4

---
+++ 2015/Day4/Day4.2.py
@@ -0,0 +1,32 @@
+import hashlib
+
+def part1():
+  # Get Input Data
+  f = open("2015/Day4/input.txt", "r")
+  seed = f.read()
+  f.close()
+
+  # TEST
+  # seed = 'abcdef'
+  # string = f'{seed}609043'
+  # seed = 'pqrstuv'
+  # string = f'{seed}1048970'
+  # return hashlib.md5(string.encode()).hexdigest()
+
+  curr = ['z' for el in range(6)]
+
+  result = [None, None] # [hash, seed]
+  i = 0
+
+  result = ''.join(str(e) for e in curr)
+
+  while result[:6] != '000000':
+    string = f'{seed}{i}'
+    result = hashlib.md5(string.encode()).hexdigest()
+    print(f'{i}-{seed}-👻-{result[:6]}--{result}')
+    i += 1
+
+  return [result, seed, i - 1]
+
+result = part1()
+print(result)
